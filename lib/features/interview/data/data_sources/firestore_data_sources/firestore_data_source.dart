3c679cfd021080a5449df6fe001f9b0479615206
Andrey-1611 <16112009nap@mail.ru>
2025-07-04 16:22:14 +0300

    init every user

--- lib/features/interview/data/data_sources/firestore_data_sources/firestore_data_source.dart
+++ lib/features/interview/data/data_sources/firestore_data_sources/firestore_data_source.dart
@@ -1,28 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../models/interview.dart';
 import 'firestore_data_source_interface.dart';
 
 class FirestoreDataSource implements LocalDataSourceInterface {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   late final String _userId;
+  late final Future<void> _init;
 
   FirestoreDataSource() {
-    _userId = 'user_${DateTime.now().millisecondsSinceEpoch}';
+    _init = _initUserId();
   }
 
-  CollectionReference get _userInterviews {
+  Future<void> _initUserId() async{
+    final db = await SharedPreferences.getInstance();
+    String? userId = db.getString('userId');
+    if (userId == null) {
+      userId = '${DateTime.now().millisecondsSinceEpoch}';
+      await db.setString('userId', userId);
+    }
+    _userId = userId;
+  }
+
+  Future<CollectionReference<Object?>> get _userInterviews async {
+    await _init;
     return _firestore.collection('users/$_userId/interviews');
   }
 
 
   @override
   Future<void> addInterview(Interview interview) async {
-    await _userInterviews.add(interview.toMap());
+    final interviews = await _userInterviews;
+    await interviews.add(interview.toMap());
   }
 
   @override
   Future<List<Interview>> showInterviews() async {
-    final data = await _userInterviews.get();
+    final interviews = await _userInterviews;
+    final data = await interviews.get();
     final questions =
         data.docs
             .map((interview) => Interview.fromMap(interview.data() as Map<String, dynamic>))
