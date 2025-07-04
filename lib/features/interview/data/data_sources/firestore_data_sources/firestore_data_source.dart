c0ce226519998c48422aa5140dc25c997e0e5884
Andrey-1611 <16112009nap@mail.ru>
2025-07-04 12:49:01 +0300

    add user id

--- lib/features/interview/data/data_sources/firestore_data_sources/firestore_data_source.dart
+++ lib/features/interview/data/data_sources/firestore_data_sources/firestore_data_source.dart
@@ -3,18 +3,26 @@ import '../../models/interview.dart';
 import 'firestore_data_source_interface.dart';
 
 class FirestoreDataSource implements LocalDataSourceInterface {
-  final CollectionReference interviews = FirebaseFirestore.instance.collection(
-    'interviews',
-  );
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  late final String _userId;
+
+  FirestoreDataSource() {
+    _userId = 'user_${DateTime.now().millisecondsSinceEpoch}';
+  }
+
+  CollectionReference get _userInterviews {
+    return _firestore.collection('users/$_userId/interviews');
+  }
+
 
   @override
   Future<void> addInterview(Interview interview) async {
-    await interviews.add(interview.toMap());
+    await _userInterviews.add(interview.toMap());
   }
 
   @override
   Future<List<Interview>> showInterviews() async {
-    final data = await interviews.get();
+    final data = await _userInterviews.get();
     final questions =
         data.docs
             .map((interview) => Interview.fromMap(interview.data() as Map<String, dynamic>))
